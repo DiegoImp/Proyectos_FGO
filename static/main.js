74bd6ec494a3b999f8b5fd8c4486e0bdc4680517
Diego Impriglio <diegoimptrabajo@gmail.com>
2025-10-28 17:16:15 -0300

    side bar and filter button

--- static/main.js
+++ static/main.js
@@ -2,6 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const searchbar = document.getElementById('search-bar'); // tomamos la barra de busqueda
     const servantLinks = document.querySelectorAll('.details_link'); // tomamos los link
+    const filterbutton = document.getElementById('button-filter'); //tomamos el boton de filtros
+    const sidebar = document.getElementById('sidebar'); //tomamos el sidebar
+
+
+    
+
+    filterbutton.addEventListener('click', () => {
+        sidebar.classList.toggle('sidebar-visible');
+
+    });
+
 
     searchbar.addEventListener('input', () => {   // esperamos un input en la barra
         const textoBusqueda = searchbar.value.toLowerCase();     // extraemos la cadena de caracteres en lower case 
