e67914ea634fe8954f8b8b7f32a06414753f0a5e
Diego Impriglio <diegoimptrabajo@gmail.com>
2025-10-29 13:08:00 -0300

    change html name and format js

--- static/main.js
+++ static/main.js
@@ -1,31 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const searchbar = document.getElementById("search-bar"); // tomamos la barra de busqueda
+  const servantLinks = document.querySelectorAll(".details_link"); // tomamos los link
+  const filterbutton = document.getElementById("button-filter"); //tomamos el boton de filtros
+  const sidebar = document.getElementById("sidebar"); //tomamos el sidebar
 
-    const searchbar = document.getElementById('search-bar'); // tomamos la barra de busqueda
-    const servantLinks = document.querySelectorAll('.details_link'); // tomamos los link
-    const filterbutton = document.getElementById('button-filter'); //tomamos el boton de filtros
-    const sidebar = document.getElementById('sidebar'); //tomamos el sidebar
+  filterbutton.addEventListener("click", () => {
+    sidebar.classList.toggle("sidebar-visible");
+  });
 
+  searchbar.addEventListener("input", () => {
+    // esperamos un input en la barra
+    const textoBusqueda = searchbar.value.toLowerCase(); // extraemos la cadena de caracteres en lower case
 
-    
+    servantLinks.forEach((link) => {
+      const servantCard = link.querySelector(".servant_card");
+      const nombreDeLaTarjeta = servantCard.dataset.name;
 
-    filterbutton.addEventListener('click', () => {
-        sidebar.classList.toggle('sidebar-visible');
-
+      if (nombreDeLaTarjeta.includes(textoBusqueda)) {
+        link.style.display = "block";
+      } else {
+        link.style.display = "none";
+      }
     });
-
-
-    searchbar.addEventListener('input', () => {   // esperamos un input en la barra
-        const textoBusqueda = searchbar.value.toLowerCase();     // extraemos la cadena de caracteres en lower case 
-
-        servantLinks.forEach(link => {
-            const servantCard = link.querySelector('.servant_card');
-            const nombreDeLaTarjeta = servantCard.dataset.name; 
-
-            if (nombreDeLaTarjeta.includes(textoBusqueda)) {
-                link.style.display = 'block';
-            } else {
-                link.style.display = 'none';
-            }
-        });
-    });
+  });
 });
